7093c191ec442802e1c4624beef15c59b1edcbba
mderoberts <mderoberts@users.noreply.github.com>
2017-10-29 16:25:26 -0400

    app.js restore, auth0 reconfig structure

--- client/src/App.js
+++ client/src/App.js
@@ -6,89 +6,20 @@ import UserHome from "./pages/UserHome";
 import NoMatch from "./pages/NoMatch";
 import Baseline from "./pages/Baseline";
 import Nav from "./components/Nav";
-import Auth from './Auth/Auth.js';
+import Auth0 from "./components/auth/auth.js";
 
-// const App = () =>
+const App = () =>
 
-const auth = new Auth();
-auth.login();
-
-class App extends Component {
-  goTo(route) {
-    this.props.history.replace(`/${route}`)
-  }
-
-  login() {
-    this.props.auth.login();
-  }
-
-  logout() {
-    this.props.auth.logout();
-  }
-
-  render() {
-    const { isAuthenticated } = this.props.auth;
-
-    return (
-      <div>
-        <Navbar fluid>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Auth0 - React</a>
-            </Navbar.Brand>
-            <Button
-              bsStyle="primary"
-              className="btn-margin"
-              onClick={this.goTo.bind(this, 'home')}
-            >
-              Home
-            </Button>
-            {
-              !isAuthenticated() && (
-                  <Button
-                    bsStyle="primary"
-                    className="btn-margin"
-                    onClick={this.login.bind(this)}
-                  >
-                    Log In
-                  </Button>
-                )
-            }
-            {
-              isAuthenticated() && (
-                  <Button
-                    bsStyle="primary"
-                    className="btn-margin"
-                    onClick={this.logout.bind(this)}
-                  >
-                    Log Out
-                  </Button>
-                )
-            }
-          </Navbar.Header>
-        </Navbar>
-        <Switch>
-          <Route exact path="/" component={SignIn} />
-          <Route exact path="/workout" component={UserHome} />
-          <Route component={NoMatch} />
-        </Switch>
-
-      </div>
-    );
-  }
-}
+  
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/workout" component={UserHome} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>;
 
 export default App;
-  
-//   <Router>
-//     <div>
-//       <Nav />
-//       <Switch>
-//         <Route exact path="/" component={SignIn} />
-//         <Route exact path="/workout" component={UserHome} />
-//         <Route component={NoMatch} />
-//       </Switch>
-//     </div>
-//   </Router>;
-
-// export default App;
